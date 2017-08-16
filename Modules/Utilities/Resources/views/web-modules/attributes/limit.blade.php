b5d19e13e9599fb6ea48cad3a45dc2a4849a26d6
Abdulbasset <abdulbasset.nasser.92@gmail.com>
2017-08-16 22:26:23 +0300

    AA1992 Show Modules Attributes With styling [Done] And start with Saving [not done]

--- Modules/Utilities/Resources/views/web-modules/attributes/limit.blade.php
+++ Modules/Utilities/Resources/views/web-modules/attributes/limit.blade.php
@@ -1,1 +1,7 @@
-<input type="number">
+<div class="form-group ">
+    <label class="col-lg-3 control-label" for="limit">Limit  <span class="text-danger">*</span>  </label>
+    <div class="col-lg-8">
+        <input type="number" id="limit" name="webModule[limit]" placeholder="Limit" class="form-control required req">
+        <div id="error_limit"></div>
+    </div>
+</div>
--- Modules/Utilities/Resources/views/web-modules/attributes/width.blade.php
+++ Modules/Utilities/Resources/views/web-modules/attributes/width.blade.php
@@ -1,1 +1,7 @@
-<input type="number" class="form-control">
+<div class="form-group ">
+    <label class="col-lg-3 control-label" for="width">Width  <span class="text-danger">*</span>  </label>
+    <div class="col-lg-8">
+        <input type="number" id="width" name="webModule[width]" placeholder="Width" class="form-control required req">
+        <div id="error_width"></div>
+    </div>
+</div>
