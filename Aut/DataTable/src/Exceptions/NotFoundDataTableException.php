f01f495f24448e8c1540361e9dd6a5fa732d69e8
Abdulbasset <abdulbasset.nasser.92@gmail.com>
2017-08-21 23:04:37 +0300

    AA1992 Add New Exception UndefinedMethodCallException Used in BaseDataTableMaker

--- Aut/DataTable/src/Exceptions/NotFoundDataTableException.php
+++ Aut/DataTable/src/Exceptions/NotFoundDataTableException.php
@@ -3,9 +3,8 @@
 namespace Aut\DataTable\Exceptions;
 
 
-use Intervention\Image\Exception\RuntimeException;
 
-class NotFoundDataTableException extends RuntimeException
+class NotFoundDataTableException extends \RuntimeException
 {
     protected $message = 'This model not registered';
 }
