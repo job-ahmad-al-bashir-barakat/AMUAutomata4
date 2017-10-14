769a2bef5ec4404e97dd5c3fd4f202afeed958d4
= <=>
2017-10-14 20:13:19 +0300

    Generate table seed

--- database/seeds/MenuTableNameLangsTableSeeder.php
+++ database/seeds/MenuTableNameLangsTableSeeder.php
@@ -16,6 +16,88 @@ class MenuTableNameLangsTableSeeder extends Seeder
 
         \DB::table('menu_table_name_langs')->delete();
         
+        \DB::table('menu_table_name_langs')->insert(array (
+            0 => 
+            array (
+                'id' => 1,
+                'menu_table_id' => 1,
+                'lang_id' => 1,
+                'text' => 'Page',
+                'created_at' => '2017-09-29 21:22:05',
+                'updated_at' => '2017-09-29 21:22:05',
+                'deleted_at' => NULL,
+            ),
+            1 => 
+            array (
+                'id' => 2,
+                'menu_table_id' => 1,
+                'lang_id' => 2,
+                'text' => 'صفحة',
+                'created_at' => '2017-09-29 21:22:05',
+                'updated_at' => '2017-09-29 21:22:05',
+                'deleted_at' => NULL,
+            ),
+            2 => 
+            array (
+                'id' => 3,
+                'menu_table_id' => 2,
+                'lang_id' => 1,
+                'text' => 'Faculty',
+                'created_at' => '2017-09-29 21:22:53',
+                'updated_at' => '2017-09-29 21:22:53',
+                'deleted_at' => NULL,
+            ),
+            3 => 
+            array (
+                'id' => 4,
+                'menu_table_id' => 2,
+                'lang_id' => 2,
+                'text' => 'كلية',
+                'created_at' => '2017-09-29 21:22:53',
+                'updated_at' => '2017-09-29 21:22:53',
+                'deleted_at' => NULL,
+            ),
+            4 => 
+            array (
+                'id' => 5,
+                'menu_table_id' => 3,
+                'lang_id' => 1,
+                'text' => 'Degree',
+                'created_at' => '2017-09-29 21:23:20',
+                'updated_at' => '2017-09-29 21:23:20',
+                'deleted_at' => NULL,
+            ),
+            5 => 
+            array (
+                'id' => 6,
+                'menu_table_id' => 3,
+                'lang_id' => 2,
+                'text' => 'اختصاص',
+                'created_at' => '2017-09-29 21:23:20',
+                'updated_at' => '2017-09-29 21:23:20',
+                'deleted_at' => NULL,
+            ),
+            6 => 
+            array (
+                'id' => 7,
+                'menu_table_id' => 4,
+                'lang_id' => 1,
+                'text' => 'Department',
+                'created_at' => '2017-09-29 21:23:49',
+                'updated_at' => '2017-09-29 21:23:49',
+                'deleted_at' => NULL,
+            ),
+            7 => 
+            array (
+                'id' => 8,
+                'menu_table_id' => 4,
+                'lang_id' => 2,
+                'text' => 'قسم',
+                'created_at' => '2017-09-29 21:23:49',
+                'updated_at' => '2017-09-29 21:23:49',
+                'deleted_at' => NULL,
+            ),
+        ));
         
         
     }
