b5677f7bdb058890a0cf677b66f99d63d215975d
= <=>
2017-11-11 00:23:45 +0200

    generate Control Menu Tree

--- Modules/Utilities/Entities/ControlMenu.php
+++ Modules/Utilities/Entities/ControlMenu.php
@@ -8,33 +8,34 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ControlMenu extends \Eloquent
 {
-    use NodeTrait ,MultiLangs ,SoftDeletes;
+    use NodeTrait, MultiLangs, SoftDeletes;
 
     protected $with = ['page'];
 
-    protected $fillable = ['id' ,'control_page_id' ,'parent_id' ,'url_path' ,'order'];
+    protected $fillable = ['id', 'control_page_id', 'parent_id', 'url_path', 'order'];
 
     protected $appends = ['url_path_replace'];
 
     function page()
     {
-        return $this->belongsTo(ControlPage::class ,'control_page_id');
+        return $this->belongsTo(ControlPage::class, 'control_page_id');
     }
 
     function getUrlPathReplaceAttribute($value)
     {
         $params = request()->route()->parameters();
 
-        return replaceParamWithValue($this->url_path ,$params);
+        return replaceParamWithValue($this->url_path, $params);
     }
 
     // this is a recommended way to declare event handlers
-    protected static function boot() {
+    protected static function boot()
+    {
 
         parent::boot();
 
         //before delete() method call this
-        static::deleting(function($node) {
+        static::deleting(function ($node) {
 
             $node->page()->delete();
         });
