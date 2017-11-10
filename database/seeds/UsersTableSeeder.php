e5b1f2ab1a888eced1b8a72a5d5cf8dbda37177c
= <=>
2017-11-10 21:22:50 +0200

    finish fix some issue

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,6 +16,20 @@ class UsersTableSeeder extends Seeder
 
         \DB::table('users')->delete();
         
+        \DB::table('users')->insert(array (
+            0 => 
+            array (
+                'id' => 1,
+                'name' => 'AUT405',
+                'email' => 'AUT405@automata4.com',
+                'password' => '$2y$10$xaEezRxChZfy6kMMB1/NDeLAIXpUTaUUHiW4/YHe69gPDeTbYSicW',
+                'image_id' => NULL,
+                'deleted_at' => NULL,
+                'remember_token' => NULL,
+                'created_at' => '2017-11-10 19:22:23',
+                'updated_at' => '2017-11-10 19:22:23',
+            ),
+        ));
         
         
     }
