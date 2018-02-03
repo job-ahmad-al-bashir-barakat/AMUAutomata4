8724018b953d67437976a8e8eb49422df9315cdb
Abdulbasset <abdulbasset.nasser.92@gmail.com>
2018-02-03 07:29:38 +0200

    AA1992 temp footer

--- resources/views/theme/footer.blade.php
+++ resources/views/theme/footer.blade.php
@@ -5,11 +5,11 @@
             <div class="col-sm-6 col-md-3">
                 <div class="widget dark">
                     <img class="mt-5 mb-20" alt="" src="{{ asset('images/logo-white-footer.png') }}">
-                    <p>203, Envato Labs, Behind Alis Steet, Melbourne, Australia.</p>
+                    <p>Al Andalus University For Medical Sciences.</p>
                     <ul class="list-inline mt-5">
                         <li class="m-0 pl-10 pr-10"> <i class="fa fa-phone text-theme-color-2 mr-5"></i> <a class="text-gray" href="#">123-456-789</a> </li>
-                        <li class="m-0 pl-10 pr-10"> <i class="fa fa-envelope-o text-theme-color-2 mr-5"></i> <a class="text-gray" href="#">contact@yourdomain.com</a> </li>
-                        <li class="m-0 pl-10 pr-10"> <i class="fa fa-globe text-theme-color-2 mr-5"></i> <a class="text-gray" href="#">www.yourdomain.com</a> </li>
+                        <li class="m-0 pl-10 pr-10"> <i class="fa fa-envelope-o text-theme-color-2 mr-5"></i> <a class="text-gray" href="#">contact@alandalus.com</a> </li>
+                        {{--<li class="m-0 pl-10 pr-10"> <i class="fa fa-globe text-theme-color-2 mr-5"></i> <a class="text-gray" href="#">www.yourdomain.com</a> </li>--}}
                     </ul>
                 </div>
             </div>
