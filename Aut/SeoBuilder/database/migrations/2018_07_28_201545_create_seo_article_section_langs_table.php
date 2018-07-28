b50192ed81c05cd23ecee864df4c456e3173461f
Abdulbasset <abdulbasset.nasser.92@gmail.com>
2018-07-28 23:18:00 +0300

    AA1992 Generate Migration file for seo

---
+++ Aut/SeoBuilder/database/migrations/2018_07_28_201545_create_seo_article_section_langs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateSeoArticleSectionLangsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('seo_article_section_langs', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('seo_id')->unsigned()->index('seo_id');
+			$table->integer('lang_id')->unsigned()->index('lang_id');
+			$table->string('text', 191);
+			$table->timestamps();
+			$table->softDeletes();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('seo_article_section_langs');
+	}
+
+}
