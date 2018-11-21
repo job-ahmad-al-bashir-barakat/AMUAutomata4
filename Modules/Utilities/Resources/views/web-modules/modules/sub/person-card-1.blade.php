0bd0f1edafe04cb484528496fd352c21c5019cbb
AA1992 <abdulbasset.nasser.92@gmail.com>
2018-11-21 17:30:16 +0200

    Fix Issue

--- Modules/Utilities/Resources/views/web-modules/modules/sub/person-card-1.blade.php
+++ Modules/Utilities/Resources/views/web-modules/modules/sub/person-card-1.blade.php
@@ -5,8 +5,9 @@
         </div>
         <div class="team-bottom-part border-bottom-theme-color-2-2px bg-lighter border-1px text-center p-10 pt-20 pb-10">
             <h4 class="text-uppercase font-raleway font-weight-600 m-0">
-                <a class="text-theme-color-2" href="{{ RouteUrls::universityStaff(getSlug($person->id, $person->lang_name[$lang]->text)) }}"> {{ $person->lang_name[$lang]->text }}</a></h4>
-            <h5 class="text-theme-color">{{ $person->jobTitle->lang_name[$lang]->text }}</h5>
+                <a class="text-theme-color-2" href="{{ RouteUrls::universityStaff(getSlug($person->id, $person->lang_name[$lang]->text)) }}"> {{ $person->lang_name[$lang]->text }}</a>
+            </h4>
+            @if($person->jobTitle->count())<h5 class="text-theme-color">{{ $person->jobTitle->lang_name[$lang]->text }}</h5>@endif
             <ul class="styled-icons icon-sm icon-dark icon-theme-colored">
                 @if($person->contact && $person->contact->socialNetwork)
                     @include('utilities::web-modules.modules.sub.social-network', ['socialNetworks' => $person->contact->socialNetwork])
