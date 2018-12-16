9be080956dbe3cfdd8cba15069e0f7ef6a65a745
AA1992 <abdulbasset.nasser.92@gmail.com>
2018-12-16 17:16:25 +0200

    Refactoring Person page

--- Modules/Utilities/Resources/views/web-modules/modules/person-page.blade.php
+++ Modules/Utilities/Resources/views/web-modules/modules/person-page.blade.php
@@ -4,11 +4,6 @@
 
 <div class="col-md-{{ $data['width'] }}">
     <div class="row">
-        <div class="col-md-4">
-            <div class="thumb">
-                <img src="{{ asset($person->image_path['lg']) }}" alt="">
-            </div>
-        </div>
         <div class="col-md-8">
             <h4 class="name font-24 mt-0 mb-0">{{ $person->lang_name[$lang]->text }}</h4>
             <h5 class="mt-5 text-theme-color-2">{{ $person->position->lang_name[$lang]->text }}</h5>
@@ -21,59 +16,33 @@
                 @endif
             </ul>
         </div>
+        <div class="col-md-4">
+            <div class="thumb">
+                <img src="{{ asset($person->image_path['lg']) }}" alt="">
+            </div>
+        </div>
     </div>
 </div>
 <div class="row mt-30">
-    <div class="col-md-4">
-        <h4 class="line-bottom">@lang('app.about_me'):</h4>
-        <div class="volunteer-address">
-            <ul>
-                @if($person->lang_experience)
-                <li>
-                    <div class="bg-light media border-bottom p-15 mb-20">
-                        <div class="media-left">
-                            <i class="pe-7s-pen text-theme-colored font-24 mt-5"></i>
+    @if($person->lang_experience)
+        <div class="col-md-4">
+            <h4 class="line-bottom">@lang('app.experience'):</h4>
+            <div class="volunteer-address">
+                <ul>
+                    <li>
+                        <div class="bg-light media border-bottom p-15 mb-20">
+                            <div class="media-left">
+                                <i class="pe-7s-pen text-theme-colored font-24 mt-5"></i>
+                            </div>
+                            <div class="media-body">
+                                <p>{!! $person->lang_experience[$lang]->text !!}</p>
+                            </div>
                         </div>
-                        <div class="media-body">
-                            <h5 class="mt-0 mb-0">@lang('app.experience'):</h5>
-                            <p>{!! $person->lang_experience[$lang]->text !!}</p>
-                        </div>
-                    </div>
-                </li>
-                @endif
-                @if($person->contact->lang_address->count())
-                <li>
-                    <div class="bg-light media border-bottom p-15 mb-20">
-                        <div class="media-left">
-                            <i class="fa fa-map-marker text-theme-colored font-24 mt-5"></i>
-                        </div>
-                        <div class="media-body">
-                            <h5 class="mt-0 mb-0">@lang('app.address'):</h5>
-                            <p>{{ $person->contact->lang_address[$lang]->text }}</p>
-                        </div>
-                    </div>
-                </li>
-                @endif
-                @if($person->contact->phone || $person->contact->email || $person->contact->mobile)
-                <li>
-                    <div class="bg-light media border-bottom p-15">
-                        <div class="media-left">
-                            <i class="fa fa-phone text-theme-colored font-24 mt-5"></i>
-                        </div>
-                        <div class="media-body">
-                            <h5 class="mt-0 mb-0">@lang('admin::app.contact'):</h5>
-                            <p>
-                                @if($person->contact->phone)<span>@lang('admin::app.phone'):</span> {{ $person->contact->phone }}<br>@endif
-                                @if($person->contact->email)<span>@lang('admin::app.email'):</span> {{ $person->contact->email }}<br>@endif
-                                @if($person->contact->mobile)<span>@lang('admin::app.mobile'):</span> {{ $person->contact->mobile }}<br>@endif
-                            </p>
-                        </div>
-                    </div>
-                </li>
-                @endif
-            </ul>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </div>
+    @endif
     @if($person->contact->gelocation)
     <div class="col-md-4">
         <h4 class="line-bottom">@lang('app.find_location'):</h4>
@@ -96,34 +65,41 @@
         </div>
     </div>
     @endif
-    {{--<div class="col-md-4">
-        <div class="clearfix">
-            <h4 class="line-bottom">Quick Contact:</h4>
+    <div class="col-md-4 pull-{{ $dir === 'rtl'? 'left' : 'right' }}">
+        <h4 class="line-bottom">@lang('admin::app.contact'):</h4>
+        <div class="volunteer-address">
+            <ul>
+                @if($person->contact->lang_address->count())
+                    <li>
+                        <div class="bg-light media border-bottom p-15 mb-20">
+                            <div class="media-left">
+                                <i class="fa fa-map-marker text-theme-colored font-24 mt-5"></i>
+                            </div>
+                            <div class="media-body">
+                                <h5 class="mt-0 mb-0">@lang('app.address'):</h5>
+                                <p>{{ $person->contact->lang_address[$lang]->text }}</p>
+                            </div>
+                        </div>
+                    </li>
+                @endif
+                @if($person->contact->phone || $person->contact->email || $person->contact->mobile)
+                    <li>
+                        <div class="bg-light media border-bottom p-15">
+                            <div class="media-left">
+                                <i class="fa fa-phone text-theme-colored font-24 mt-5"></i>
+                            </div>
+                            <div class="media-body">
+{{--                                <h5 class="mt-0 mb-0">@lang('admin::app.contact'):</h5>--}}
+                                <p>
+                                    @if($person->contact->phone)<span>@lang('admin::app.phone'):</span> {{ $person->contact->phone }}<br>@endif
+                                    @if($person->contact->email)<span>@lang('admin::app.email'):</span> {{ $person->contact->email }}<br>@endif
+                                    @if($person->contact->mobile)<span>@lang('admin::app.mobile'):</span> {{ $person->contact->mobile }}<br>@endif
+                                </p>
+                            </div>
+                        </div>
+                    </li>
+                @endif
+            </ul>
         </div>
-        <form id="contact-form" class="contact-form-transparent">
-            <div class="row">
-                <div class="col-sm-12">
-                    <div class="form-group">
-                        <input type="text" placeholder="Enter Name" id="contact_name" name="contact_name" required="" class="form-control">
-                    </div>
-                </div>
-                <div class="col-sm-6">
-                    <div class="form-group">
-                        <input type="text" placeholder="Enter Email" id="contact_email" name="contact_email" class="form-control" required="">
-                    </div>
-                </div>
-                <div class="col-sm-6">
-                    <div class="form-group">
-                        <input type="text" placeholder="Enter Subject" id="contact_subject" name="contact_subject" class="form-control" required="">
-                    </div>
-                </div>
-            </div>
-            <div class="form-group">
-                <textarea rows="5" placeholder="Enter Message" id="contact_message" name="contact_message" required class="form-control"></textarea>
-            </div>
-            <div class="form-group">
-                <button data-loading-text="Please wait..." class="btn btn-flat btn-dark btn-theme-colored mt-5" type="submit">Send your message</button>
-            </div>
-        </form>
-    </div>--}}
+    </div>
 </div>
