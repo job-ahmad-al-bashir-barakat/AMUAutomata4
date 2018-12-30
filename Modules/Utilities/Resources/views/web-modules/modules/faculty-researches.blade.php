181e059ba0c2deb42e20b4ef5ea1b0042a2543f1
AA1992 <abdulbasset.nasser.92@gmail.com>
2018-12-30 16:15:12 +0200

    Showing instructor if has research

--- Modules/Utilities/Resources/views/web-modules/modules/faculty-researches.blade.php
+++ Modules/Utilities/Resources/views/web-modules/modules/faculty-researches.blade.php
@@ -1,5 +1,6 @@
 <div class="row">
     @foreach($data['instructors'] as $instructor)
+        @if($instructor->researches && $instructor->researches->count())
         <div class="col-md-4">
             <div class="esc-heading heading-line-bottom lr-line left-heading">
                 <h4>{{ $instructor->lang_name[$lang]->text }}</h4>
@@ -10,5 +11,6 @@
                 @endforeach
             </ul>
         </div>
+        @endif
     @endforeach
 </div>
