1b73e52281d77cc33cd0670de15f693da381128f
AA1992 <abdulbasset.nasser.92@gmail.com>
2019-01-01 12:44:51 +0200

    Make Block Text Optional

--- Modules/Utilities/Database/Migrations/2017_09_28_190355_create_block_detail_text_langs_table.php
+++ Modules/Utilities/Database/Migrations/2017_09_28_190355_create_block_detail_text_langs_table.php
@@ -17,7 +17,7 @@ class CreateBlockDetailTextLangsTable extends Migration {
 			$table->increments('id');
 			$table->integer('block_detail_id')->unsigned()->index('block_detail_id');
 			$table->integer('lang_id')->unsigned()->index('lang_id');
-			$table->string('text', 350);
+			$table->string('text', 350)->nullable();
 			$table->timestamps();
 			$table->softDeletes();
 		});
