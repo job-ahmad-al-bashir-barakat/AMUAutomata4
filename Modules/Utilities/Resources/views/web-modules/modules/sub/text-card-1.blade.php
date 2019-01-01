a6b3b92bf57a6f1ca034bb2f5553a6102b8a3749
AA1992 <abdulbasset.nasser.92@gmail.com>
2019-01-01 09:44:25 +0200

    Fix Show Attribute

--- Modules/Utilities/Resources/views/web-modules/modules/sub/text-card-1.blade.php
+++ Modules/Utilities/Resources/views/web-modules/modules/sub/text-card-1.blade.php
@@ -1,4 +1,4 @@
-<div class="col-md-12">
+<div class="col-md-{{ $width ?? 12 }}">
     <div class="icon-box left media bg-deep p-30 mb-20"> <a class="media-left pull-left" href="{{ $textCard->url }}"> <i class="{{ $textCard->icon->code }} text-theme-colored"></i></a>
         <div class="media-body"> <strong>{{ $textCard->lang_title[$lang]->text }}</strong>
             <p>#405, Lan Streen, Los Vegas, USA</p>
--- Modules/Utilities/Resources/views/web-modules/modules/text-card-1.blade.php
+++ Modules/Utilities/Resources/views/web-modules/modules/text-card-1.blade.php
@@ -1,7 +1,7 @@
 <div class="col-md-{{ $data['width'] }}">
     <div class="row">
         @foreach($data['text_cards_1'] as $textCard)
-        @include('utilities::web-modules.modules.sub.text-card-1')
+        @include('utilities::web-modules.modules.sub.text-card-1', ['width' => (12/$data['show'])])
         @endforeach
     </div>
 </div>
