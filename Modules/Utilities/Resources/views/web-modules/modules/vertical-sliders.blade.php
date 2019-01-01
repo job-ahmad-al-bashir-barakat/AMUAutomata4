a191f191616a5b5a0a76daf8fc6e4e5a36eae42d
AA1992 <abdulbasset.nasser.92@gmail.com>
2019-01-01 13:07:40 +0200

    Activate Show attribute and icon in vertical slider

--- Modules/Utilities/Resources/views/web-modules/modules/vertical-sliders.blade.php
+++ Modules/Utilities/Resources/views/web-modules/modules/vertical-sliders.blade.php
@@ -1,6 +1,6 @@
 <div class="col-md-{{ $data['width'] }} wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0.3s">
-    @include('utilities::web-modules.modules.sub.title', ['title' => $data['title'][$lang], 'h' => '3', 'font' => ''])
-    <div class="bxslider bx-nav-top">
+    @include('utilities::web-modules.modules.sub.title', ['title' => $data['title'][$lang], 'h' => '3', 'font' => '', 'icon' => $data['icon']->code ?? ''])
+    <div class="bxslider bx-nav-top" data-count="{{ $data['show'] ?? '2' }}">
         @php($borderDir = 'left')
         @foreach($data['vertical_sliders']->verticalSliderDetails()->with(['image'])->get() as $slider)
         @include('utilities::web-modules.modules.sub.vertical-slider', ['borderDir' => $borderDir, 'slider' => $slider])
--- Modules/Utilities/WebModules/Modules/VerticalSliderModule.php
+++ Modules/Utilities/WebModules/Modules/VerticalSliderModule.php
@@ -3,6 +3,8 @@
 namespace Modules\Utilities\WebModules\Modules;
 
 
+use Modules\Utilities\Entities\Icon;
+
 class VerticalSliderModule extends Module
 {
 
@@ -11,5 +13,14 @@ class VerticalSliderModule extends Module
     public $code = 'vertical_sliders';
 
     public $viewName = 'vertical-sliders';
+
+    public function getModuleData($data)
+    {
+        $data = parent::getModuleData($data);
+        if ($data['icon']) {
+            $data['icon'] = Icon::query()->whereIn('id', array_keys($data['icon'][0]))->get()->first();
+        }
+        return $data;
+    }
 
 }
