11ea0bc1ea63a6222cf984933d0205ef11ae6699
AA1992 <abdulbasset.nasser.92@gmail.com>
2019-01-08 14:57:20 +0200

    Update links position

--- resources/views/theme/footer.blade.php
+++ resources/views/theme/footer.blade.php
@@ -1,11 +1,31 @@
+@php($links = $footer->footerLinks->split(2))
 <footer id="footer" class="footer pb-0" {{--data-bg-img="images/footer-bg.png"--}} data-bg-color="#25272e" style="{{--background-image: url(&quot;images/footer-bg.png&quot;);--}} background-position: initial !important; background-size: initial !important; background-repeat: initial !important; background-attachment: initial !important; background-origin: initial !important; background-clip: initial !important; background-color: rgb(37, 39, 46) !important;">
     @if($footer)
         <div class="container pt-70 pb-40">
             <div class="row multi-row-clearfix">
-                <div class="col-md-6 col-md-offset-3 text-center">
+
+                <div class="col-md-3 text-center">
+                    <div class="widget dark">
+                        <div class="tags">
+                            @foreach($links->get('0', []) as $link)
+                                <a href="{{ $link->lang_url[$lang]->text }}">{{ $link->lang_text[$lang]->text }}</a>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 text-center">
                     @php($logoPath = $footer->logo_path ?? ($logoPath ?? 'images/logo-wide-'.$lang.'.png'))
                     <img alt="" src="{{ asset($logoPath) }}" style="width: 200px;">
                     @if($footer->lang_text[$lang])<p class="font-12 mt-20 mb-10">{{ $footer->lang_text[$lang]->text }}</p>@endif
+                </div>
+                <div class="col-md-3 text-center">
+                    <div class="widget dark">
+                        <div class="tags">
+                            @foreach($links->get('1', []) as $link)
+                                <a href="{{ $link->lang_url[$lang]->text }}">{{ $link->lang_text[$lang]->text }}</a>
+                            @endforeach
+                        </div>
+                    </div>
                 </div>
                 <div class="col-md-6 col-md-offset-3 text-center">
                     <ul class="styled-icons icon-dark mt-20 icon-circled">
@@ -14,15 +34,6 @@
                         <li><a target="_blank" href="{{setting('google_account')->value}}" data-bg-color="#A11312" style="background: rgb(161, 19, 18) !important;"><i class="fa fa-google-plus"></i></a></li>
                         <li><a target="_blank" href="{{setting('youtube_channel')->value}}" data-bg-color="#C22E2A" style="background: rgb(194, 46, 42) !important;"><i class="fa fa-youtube"></i></a></li>
                     </ul>
-                </div>
-                <div class="col-md-6 col-md-offset-3 text-center">
-                    <div class="widget dark">
-                        <div class="tags">
-                            @foreach($footer->footerLinks as $link)
-                                <a href="{{ $link->lang_url[$lang]->text }}">{{ $link->lang_text[$lang]->text }}</a>
-                            @endforeach
-                        </div>
-                    </div>
                 </div>
             </div>
             <div class="row">
@@ -56,5 +67,4 @@
         </div>
     </div>
 </footer>
-
 <a class="scrollToTop" href="#"><i class="fa fa-angle-up"></i></a>
