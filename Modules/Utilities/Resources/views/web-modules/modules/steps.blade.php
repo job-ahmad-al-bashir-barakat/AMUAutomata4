77609d9b1e6adba98ed1b8ee9098765fd8f28fc4
AA1992 <abdulbasset.nasser.92@gmail.com>
2019-01-16 08:40:03 +0200

    Steps Expand conflict fixed

--- Modules/Utilities/Resources/views/web-modules/modules/steps.blade.php
+++ Modules/Utilities/Resources/views/web-modules/modules/steps.blade.php
@@ -5,8 +5,8 @@
         @php($firstStep = $data['steps']->pull(0))
         @if($firstStep)
             <div class="panel">
-                <div class="panel-title"> <a class="active" data-parent="#accordion1" data-toggle="collapse" href="#accordion11" aria-expanded="true"> <span class="open-sub"></span> {{ $firstStep->lang_title[$lang]->text }}</a> </div>
-                <div id="accordion11" class="panel-collapse collapse in" role="tablist" aria-expanded="true">
+                <div class="panel-title"> <a class="active" data-parent="#accordion_{{ $firstStep->id }}" data-toggle="collapse" href="#accordion11" aria-expanded="true"> <span class="open-sub"></span> {{ $firstStep->lang_title[$lang]->text }}</a> </div>
+                <div id="accordion_{{ $firstStep->id }}" class="panel-collapse collapse in" role="tablist" aria-expanded="true">
                     <div class="panel-content">
                         {!! $firstStep->lang_text[$lang]->text !!}
                     </div>
@@ -15,8 +15,8 @@
         @endif
         @foreach($data['steps'] as $step)
             <div class="panel">
-                <div class="panel-title"> <a data-parent="#accordion1" data-toggle="collapse" href="#accordion12" class="collapsed" aria-expanded="false"> <span class="open-sub"></span> {{ $step->lang_title[$lang]->text }}</a> </div>
-                <div id="accordion12" class="panel-collapse collapse" role="tablist" aria-expanded="false" style="height: 0px;">
+                <div class="panel-title"> <a data-parent="#accordion_{{ $step->id }}" data-toggle="collapse" href="#accordion12" class="collapsed" aria-expanded="false"> <span class="open-sub"></span> {{ $step->lang_title[$lang]->text }}</a> </div>
+                <div id="accordion_{{ $step->id }}" class="panel-collapse collapse" role="tablist" aria-expanded="false" style="height: 0px;">
                     <div class="panel-content">
                         {!! $step->lang_text[$lang]->text !!}
                     </div>
