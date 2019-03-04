97f8daa56ff3f1347b71c50b21b79eb3f73e2ef7
AA1992 <abdulbasset.nasser.92@gmail.com>
2019-03-04 10:02:23 +0200

    Fix Undefined translation

--- Modules/Utilities/Resources/views/web-modules/attributes/texteditor.blade.php
+++ Modules/Utilities/Resources/views/web-modules/attributes/texteditor.blade.php
@@ -3,7 +3,7 @@
 <div class="form-group ">
     <label class="col-lg-3 control-label" for="module-texteditor">{{ $title }} ({{ $langCode }})  <span class="text-danger">*</span>  </label>
     <div class="col-lg-8">
-        <textarea id='module-texteditor_{{ $langCode }}' class='datatable-text-editor' name="webModule[{{ $code }}][{{ $langCode }}]">{!! $data[$langCode] !!}</textarea>
+        <textarea id='module-texteditor_{{ $langCode }}' class='datatable-text-editor' name="webModule[{{ $code }}][{{ $langCode }}]">{!! $data[$langCode] ?? null !!}</textarea>
         {{--<div id="error_module-texteditor"></div>--}}
     </div>
 </div>
