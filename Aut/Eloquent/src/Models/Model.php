af0c37aff0ca0c5217d4078ffd424e8df9d6f91a
AA1992 <abdulbasset.nasser.92@gmail.com>
2019-03-10 16:19:03 +0200

    Add Activity Log

--- Aut/Eloquent/src/Models/Model.php
+++ Aut/Eloquent/src/Models/Model.php
@@ -3,8 +3,11 @@
 namespace Aut\Eloquent\Models;
 
 use Illuminate\Database\Eloquent\Model as BaseModel;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 abstract class Model extends BaseModel
 {
-
+    use LogsActivity;
+    protected static $logFillable = true;
+    protected static $logAttributesToIgnore = [ 'password'];
 }
