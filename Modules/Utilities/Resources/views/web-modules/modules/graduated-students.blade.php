d1a4cff8f7e3550f6bd973a8bdf92dafe433c88c
AA1992 <abdulbasset.nasser.92@gmail.com>
2019-04-25 12:08:41 +0300

    Add margin to student cards

--- Modules/Utilities/Resources/views/web-modules/modules/graduated-students.blade.php
+++ Modules/Utilities/Resources/views/web-modules/modules/graduated-students.blade.php
@@ -2,7 +2,7 @@
     <h2 class="mt-0 font-28 line-bottom line-height-1">@lang('app.graduate_semester') <span id="title" class="text-theme-color-2 font-weight-400"></span></h2>
     <div class="row multi-row-clearfix">
         @foreach($data['graduated'] as $student)
-            <div class="col-sm-6 col-md-3 sm-text-center mb-sm-30 filters hide filter_graduate_semester_{{ $student->academic->graduate->id }}" >
+            <div class="col-sm-6 col-md-3 sm-text-center mb-30 filters hide filter_graduate_semester_{{ $student->academic->graduate->id }}" >
                 <div class="team maxwidth400">
                     <div class="thumb"><img class="img-fullwidth" src="{{ asset($student->image_path['sm']) }}" alt=""></div>
                     <div class="content border-1px border-bottom-theme-color-2-2px p-15 bg-light clearfix" style="min-height: 165px;">
