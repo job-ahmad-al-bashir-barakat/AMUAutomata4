a3e303aaf88bba29adca1a58599f45051e1922b5
AA1992 <abdulbasset.nasser.92@gmail.com>
2019-04-25 10:55:04 +0300

    Fix Height issue

--- Modules/Utilities/Resources/views/web-modules/modules/graduated-students.blade.php
+++ Modules/Utilities/Resources/views/web-modules/modules/graduated-students.blade.php
@@ -5,7 +5,7 @@
             <div class="col-sm-6 col-md-3 sm-text-center mb-sm-30 filters hide filter_graduate_semester_{{ $student->academic->graduate->id }}" >
                 <div class="team maxwidth400">
                     <div class="thumb"><img class="img-fullwidth" src="{{ asset($student->image_path['sm']) }}" alt=""></div>
-                    <div class="content border-1px border-bottom-theme-color-2-2px p-15 bg-light clearfix">
+                    <div class="content border-1px border-bottom-theme-color-2-2px p-15 bg-light clearfix" style="min-height: 165px;">
                         <h4 class="name text-theme-color-2 mt-0">{{ $student->lang_name[app()->getLocale()]->text }}</h4>
                         <p class="mb-20">Graduation GPA: <small>{{ $student->academic->gpa }}</small></p>
                     </div>
