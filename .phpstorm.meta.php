cbca032d09181f6606f2ae79c6c423a2e440aa36
AA1992 <abdulbasset.nasser.92@gmail.com>
2019-05-01 18:01:16 +0300

    Add Session Table

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-03-28 09:14:12.
+    * Generated on 2019-05-01 14:53:26.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
