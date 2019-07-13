a85a30e56e6e6a50d2103ff608d4118516a69c9e
AA1992 <abdulbasset.nasser.92@gmail.com>
2019-07-13 10:02:07 +0300

    fix office height

--- Modules/Utilities/Resources/views/web-modules/modules/sub/office-card-1.blade.php
+++ Modules/Utilities/Resources/views/web-modules/modules/sub/office-card-1.blade.php
@@ -12,7 +12,7 @@
                     data-zoom="12"
             ></div>--}}
         </div>
-        <div class="team-bottom-part border-bottom-theme-color-2-2px bg-lighter border-1px text-center p-10 pt-20 pb-10">
+        <div class="team-bottom-part border-bottom-theme-color-2-2px bg-lighter border-1px text-center p-10 pt-20 pb-10" style="min-height: 165px;">
             <h4 class=" font-raleway font-weight-600 m-0">
                 <a class="text-theme-color-2" href="{{ RouteUrls::officePage(getSlug($office->id, $office->lang_name[$lang]->text)) }}">{{ $office->lang_name[$lang]->text }}</a>
             </h4>
